b4b3e65c6f1b59317998abd770e819bd71d3445e
Julian Gutierrez <juliangutierrez68@yahoo.com>
2016-09-22 00:11:43 -0500

    Changed value to placeholder on index.html. Added a prevent default to click event. Write paragraphs. Changed styling to added paragraphs.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
-	
+	//Cache List
+	//var $list = $('#taskList');
+
+	//Button click event handler
+	$('button').click(function(event){
+		event.preventDefault();
+		var currentTask = retrieveContent();
+		toList(currentTask);
+	});
+
+	//Function to retrieve input's value
+	function retrieveContent(){
+		var $value = $('input[name=task]').val();
+		return $value;
+	}
+
+	//Function write to list
+	function toList(task){
+		$('#taskList').append("<p>" + task + "</p>");
+		//alert(task);
+	}
 });
